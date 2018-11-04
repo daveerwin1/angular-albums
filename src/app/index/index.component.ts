694a8985deff835decbce20c4006ecfb9247fac6
daveerwin1 <stub2005@gmail.com>
2018-11-04 00:52:39 -0700

    Added: decade filter

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,13 +1,12 @@
 import { AlbumService } from '../album.service';
 import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 import {MatTable, MatTableDataSource, MatPaginator} from '@angular/material';
 import { Album } from '../Album';
 import { Genre } from '../Album';
 
 import {DataSource} from '@angular/cdk/collections';
-
 
 @Component({
   selector: 'app-index',
@@ -22,6 +21,11 @@ export class IndexComponent implements OnInit {
   dataSource = new MatTableDataSource<Album>(this.albumsArray);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   genres: any[];
+  decades: any[];
+  selectedGenre: string;
+  selectedDecade: string;
+
+  albumsObserver: Observer<Album[]>;
 
   constructor(private http: HttpClient, private albumService: AlbumService) {}
 
@@ -32,24 +36,25 @@ export class IndexComponent implements OnInit {
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
 
-    const myObserver = {
+    this.albumsObserver = {
       next: album => {
         this.albumsArray = album;
         this.dataSource.data = album;
-        this.genres = this.getGenres();
+        this.genres = this.getFilterList("genre");
+        this.decades = this.getFilterList("decade");
       },
       error: err => console.error('Observer got an error: ' + err),
       complete: () => console.log('Observer got a complete notification'),
     };
-    this.albums.subscribe(myObserver);
+    this.albums.subscribe(this.albumsObserver);
   }
 
-  getGenres(): any[] {
+  getFilterList(property): any[] {
     let unique = [];
     
     this.albumsArray.forEach(function(element) {
 
-      let str = element.genre;
+      let str = element[property];
       let result = str.split(",");
     
       result.forEach(function(element) {
@@ -61,6 +66,16 @@ export class IndexComponent implements OnInit {
       });
     });
     return unique;
+  }
+
+  filterByGenre() {
+    this.albumsArray = this.albumsArray.filter(album => album.genre.includes(this.selectedGenre));
+    this.dataSource.data = this.albumsArray;
+  }
+
+  filterByDecade() {
+    this.albumsArray = this.albumsArray.filter(album => album.decade.includes(this.selectedDecade))
+    this.dataSource.data = this.albumsArray;
   }
 
   deleteAlbum(id) {
